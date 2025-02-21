e4519358e58a0ae6532c01f2c9638af87312c90c
Galilea44 <alexandragonzalez34@aragon.unam.mx>
2025-02-21 17:42:11 -0600

    tarea_1

--- web_17feb/app6.js
+++ web_17feb/app6.js
@@ -1,7 +1,41 @@
-let resultado=true && true; //false && true 
+//EJEMPLO HECHO EN CLAE
+
+/* let resultado=true && true; //false && true 
 console.log(resultado);
 
 let resultado2 = false || true; //or
-console.log(resultado2);
+console.log(resultado2); */
 
-//programar and, or, not
+//TAREA 
+
+//AND
+console.log("----------- Operador Lógico AND -----------");
+let and0 = false && false;
+let and1 = false && true;
+let and2 = true && false;
+let and3 = true && true;
+
+console.log("|false " + "&&" + " false|" +" = " + and0)
+console.log("|false " + "&&" + " true |" +" = " + and1)
+console.log("|true " + "&&" + " false |" +" = " + and2)
+console.log("|true " + "&&" + " true  |" +" = " + and3)
+
+
+//OR
+console.log("----------- Operador Lógico OR -----------" );
+let or0 = false || false;
+let or1 = false || true;
+let or2 = true || false;
+let or3 = true || true;
+
+console.log("|false " + "||" + " false|" +" = " + or0)
+console.log("|false " + "||" + " true |" +" = " + or1)
+console.log("|true " + "||" + " false |" +" = " + or2)
+console.log("|true " + "||" + " true  |" +" = " + or3)
+
+//NOT
+console.log("----------- Operador Lógico NOT -----------" );
+let not0 = !false;
+let not1 = !true;
+console.log("|!false|" +" = " + not0)
+console.log("|!true |" +" = " + not1)
